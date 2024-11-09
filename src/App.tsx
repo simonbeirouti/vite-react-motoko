db47b363d114893e804aaeb5522398e9b9c04a5a
simonbeirouti <hello@simonbeirouti.com>
2024-11-09 16:57:08 +0700

    add tailwindcss and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import './index.css';
 import motokoLogo from './assets/motoko_moving.png';
 import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
+import { Button } from './components/ui/button';
 
 function App() {
   const { data: count, call: refetchCount } = useQueryCall({
@@ -18,38 +19,17 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://internetcomputer.org/docs/current/developer-docs/build/cdks/motoko-dfinity/motoko/"
-          target="_blank"
-        >
-          <span className="logo-stack">
-            <img
-              src={motokoShadowLogo}
-              className="logo motoko-shadow"
-              alt="Motoko logo"
-            />
-            <img src={motokoLogo} className="logo motoko" alt="Motoko logo" />
-          </span>
-        </a>
-      </div>
-      <h1>Vite + React + Motoko</h1>
-      <div className="card">
-        <button onClick={increment} disabled={loading}>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Vite + React + Motoko</h1>
+      <div className="flex flex-col items-center justify-center">
+        <Button onClick={increment} disabled={loading}>
           count is {count?.toString() ?? 'loading...'}
-        </button>
+        </Button>
         <p>
           Edit <code>backend/Backend.mo</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         Click on the Vite, React, and Motoko logos to learn more
       </p>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
@@ -32,6 +33,11 @@ export default defineConfig({
     environment('all', { prefix: 'CANISTER_' }),
     environment('all', { prefix: 'DFX_' }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: 'setupTests.ts',
